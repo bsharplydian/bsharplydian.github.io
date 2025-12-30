d7429b3f377a47e1dd040aeeb72051791527e186
Jacob <jmemmott13@gmail.com>
2025-12-30 12:19:48 -0700

    add: basic jsx layout for portfolio application

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Bio from './bio/Bio';
+import Projects from './projects/Projects';
+
+function Home() {
+    return <h1>Home</h1>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            {/* navigation */}
+            <h1>Jacob Memmott</h1>
+            <h2>Gameplay Programmer</h2>
+            <nav>
+                <Link to="/">Home</Link> | {" "}
+                <Link to="/bio">Bio</Link> | {" "}
+                <Link to="/projects">Projects</Link> | {" "}
+            </nav>
+            {/* routes */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/bio" element={<Bio />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
