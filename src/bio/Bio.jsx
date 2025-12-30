b5fa8597a77f08e0e07705f23e0b8448925c74c3
Jacob <jmemmott13@gmail.com>
2025-12-30 12:20:13 -0700

    add: basic jsx layout for portfolio application (2)

---
+++ src/bio/Bio.jsx
@@ -0,0 +1,10 @@
+function Bio() {
+    return (
+        <body>
+            <h1>Jacob Memmott</h1>
+            <p>Hello! I'm a junior studying gameplay programming at Brigham Young University. I've always been into computers and coding, and I'm eager to apply my technical skills to building impactful stories.</p>
+        </body>
+    )
+}
+
+export default Bio
---
+++ src/projects/Projects.jsx
@@ -0,0 +1,11 @@
+function Projects() {
+    return (
+        <div>
+            <div>grapple game</div>
+            <div>maze generator</div>
+            <div>dragon kisser</div>
+        </div>
+    )
+}
+
+export default Projects
